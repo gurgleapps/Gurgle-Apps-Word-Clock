96f8fe5f98f65624ed858724bb33b4b6f01fca50
Alex <alex@matica.com>
2024-01-24 15:51:07 +0000

    different colours per word

--- src/main.py
+++ src/main.py
@@ -13,10 +13,16 @@ DISPLAY_MODE_RAINBOW = 'rainbow'
 DISPLAY_MODE_SINGLE_COLOR = 'single_color'
 DISPLAY_MODE_COLOR_PER_WORD = 'color_per_word'
 
-current_display_mode = DISPLAY_MODE_SINGLE_COLOR
+current_display_mode = DISPLAY_MODE_COLOR_PER_WORD
 
 # Color data for different modes
 single_color = (0, 0, 255)
+# Color data for different words
+minute_color = (0, 255, 0)
+hour_color = (255, 0, 0)
+past_to_color = (0, 0, 255)
+# Ready to hold color data for each word
+colour_per_word_array = []
 
 clockFont = {
     'past': [0x00, 0x00, 0x1e, 0x00, 0x00, 0x00, 0x00, 0x00],
@@ -58,38 +64,70 @@ def set_time():
         print("Error setting time")
 
 def time_to_matrix():
+    global colour_per_word_array
     word = [0, 0, 0, 0, 0, 0, 0, 0]
     now = time.localtime()
     hour = (now[3])
     minute = now[4]
+    colour_per_word_array.clear()
     # round min to nearest 5
     minute = int(round(minute/5)*5)
     if minute > 0 and minute < 30:
         word = merge_chars(word, clockFont['past'])
+        colour_per_word_array = merge_color_array(colour_per_word_array, clockFont['past'], past_to_color)
     elif minute == 60:
         pass  # on the hour
     elif minute > 30:
         word = merge_chars(word, clockFont['to'])
+        colour_per_word_array = merge_color_array(colour_per_word_array, clockFont['to'], past_to_color)
         hour = hour + 1
     hour = hour % 12
     word = merge_chars(word, clockFont['h_'+str(hour)])
+    colour_per_word_array = merge_color_array(colour_per_word_array, clockFont['h_'+str(hour)], hour_color)
     if minute > 30:
         minute = 60 - minute
     if minute > 0:
         word = merge_chars(word, clockFont['m_'+str(minute)])
+        colour_per_word_array = merge_color_array(colour_per_word_array, clockFont['m_'+str(minute)], minute_color)
     if config.ENABLE_MAX7219:
         spi_matrix.show_char(word)
     if config.ENABLE_HT16K33:            
         if not i2c_matrix.show_char(i2c_matrix.reverse_char(word)):
             print("Error writing to matrix")
     if config.ENABLE_WS2812B:
-        ws2812b_matrix.show_char(word, (0, 0, 255))
-        ws2812b_matrix.show_char_with_color_array(word, ws2812b_matrix.get_rainbow_array())
+        display_fuction = display_modes.get(current_display_mode)
+        if display_fuction:
+            display_fuction(word)
 
 def merge_chars(char1, char2):
     for i in range(8):
         char1[i] |= char2[i]
     return char1
+
+def merge_color_array(color_array, char, color):
+    for i in range(8):
+        for j in range(8):
+            if char[i] & (1 << (7 - j)):
+                while len(color_array) <= i * 8 + j:
+                    color_array.append((0, 0, 0))  # Fill with default color
+                color_array[i * 8 + j] = color
+    return color_array
+
+
+def display_rainbow_mode(word):
+    ws2812b_matrix.show_char_with_color_array(word, ws2812b_matrix.get_rainbow_array())
+
+def display_single_color_mode(word):
+    ws2812b_matrix.show_char(word, single_color)
+
+def display_color_per_word_mode(word):
+    ws2812b_matrix.show_char_with_color_array(word, colour_per_word_array)
+
+display_modes = {
+    DISPLAY_MODE_RAINBOW: display_rainbow_mode,
+    DISPLAY_MODE_SINGLE_COLOR: display_single_color_mode,
+    DISPLAY_MODE_COLOR_PER_WORD: display_color_per_word_mode
+}
 
 async def main():
     while True:
